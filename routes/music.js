0f61d0b59e0cb2c0c8beee769a741b91c0e76b09
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-11 15:57:13 +0200

    Added again endpoints

--- routes/music.js
+++ routes/music.js
@@ -2,13 +2,12 @@ const express = require('express');
 const musicController = require('../controllers/music');
 const router = express.Router();
 
+// GET /music/findArtistsByGenre/:genreid
+router.get('/findArtistsByGenre/:genreid', musicController.getArtistsByGenre);
 // GET /music/:table
 router.get('/:table', musicController.getAll);
 // GET /music/:table/:id
 router.get('/:table/:id', musicController.getById);
-
-// // GET /music/findArtistsByGenre/:genreid
-// router.get('/findArtistsByGenre/:genreid', musicController.getArtistsByGenre);
 // // GET /music/findArtistsWithTrackCount
 // router.get('/findArtistsWithTrackCount', musicController.getArtistsWithTrackCount);
 
