e079f64faa47199bbba91d77a81728024d4a3102
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-11 16:02:50 +0200

    Added services done with routes

--- routes/music.js
+++ routes/music.js
@@ -2,13 +2,13 @@ const express = require('express');
 const musicController = require('../controllers/music');
 const router = express.Router();
 
+// GET /music/findArtistsWithTrackCount
+router.get('/findArtistsWithTrackCount', musicController.getArtistsWithTrackCount);
 // GET /music/findArtistsByGenre/:genreid
 router.get('/findArtistsByGenre/:genreid', musicController.getArtistsByGenre);
 // GET /music/:table
 router.get('/:table', musicController.getAll);
 // GET /music/:table/:id
 router.get('/:table/:id', musicController.getById);
-// // GET /music/findArtistsWithTrackCount
-// router.get('/findArtistsWithTrackCount', musicController.getArtistsWithTrackCount);
 
 module.exports = router;
