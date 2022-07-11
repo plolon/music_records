8cf3792ade5df3e7234423eab629eac3c414313f
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-11 17:49:37 +0200

    Added others endpoints sql services

--- controllers/music.js
+++ controllers/music.js
@@ -1,26 +1,24 @@
 const service = require('../services/music');
 
 exports.getAll = (req, res, next) => {
+  service.getAll(req, results =>{
+    res.send(results);
+  });
+};
+exports.getById = (req, res, next) => {
   service.getById(req, results =>{
     res.send(results);
   });
 };
-// exports.getById = (req, res, next) => {
-//   let query = createGetQuery(req, req.params.id);
-//   res.send(query);
-// };
-// exports.getArtistsByGenre = (req, res, next) => {
-//   let genreId = req.params.genreId;
-//   res.send(
-//     'SELECT * FROM artists WHERE artistid IN (SELECT artistid FROM albums WHERE albumid IN (SELECT albumid FROM tracks WHERE genreid IN (SELECT genreid FROM genres WHERE genreid = ' +
-//       genreId +
-//       '))) ORDER BY name'
-//   );
-// };
-// exports.getArtistsWithTrackCount = (req, res, next) => {
-//   res.send(
-//     'SELECT a.artistid, COUNT(t.trackid) AS tracksCount FROM artists AS a JOIN albums AS al ON a.artistId=al.artistId JOIN tracks as t ON al.albumid=t.albumid GROUP BY a.artistid'
-//   );
-//};
+exports.getArtistsByGenre = (req, res, next) => {
+  service.getArtistsByGenre(req, results =>{
+    res.send(results);
+  });
+};
+exports.getArtistsWithTrackCount = (req, res, next) => {
+  service.getArtistsWithTrackCount(req, results =>{
+    res.send(results);
+  });
+};
 
 
--- services/music.js
+++ services/music.js
@@ -1,25 +1,51 @@
 const db = require('../util/database');
 
+exports.getAll = (req, cb) => {
+  let query = createGetQuery(req);
+  db.all(query, [], (err, rows) => {
+    if (err) return console.error(err);
+    cb(rows);
+  });
+};
 exports.getById = (req, cb) => {
-    let results;
-    let query = createGetQuery(req, req.params.id);
-    console.log(query);
+  let query = createGetQuery(req, req.params.id);
+  db.get(query, [], (err, row) => {
+    if (err) return console.error(err);
+    cb(row);
+  });
+};
+exports.getArtistsByGenre = (req, cb) => {
+  let genreId = req.params.genreid;
+  let query =
+    'SELECT * FROM artists WHERE artistid IN (SELECT artistid FROM albums WHERE albumid IN (SELECT albumid FROM tracks WHERE genreid IN (SELECT genreid FROM genres WHERE genreid = ' +
+    genreId +
+    '))) ORDER BY name';
     db.all(query, [], (err, rows) => {
-        if(err) return console.error(err);
+        if (err) return console.error(err);
         cb(rows);
-    });
-    db.close();
-}
+      });
+};
+exports.getArtistsWithTrackCount = (req, cb) => {
+    let genreId = req.params.genreid;
+    let query =
+    'SELECT a.artistid, a.name, COUNT(t.trackid) AS tracksCount FROM artists AS a JOIN albums AS al ON a.artistId=al.artistId JOIN tracks as t ON al.albumid=t.albumid GROUP BY a.artistid';
+      db.all(query, [], (err, rows) => {
+          if (err) return console.error(err);
+          cb(rows);
+        });
+  };
 
 function createGetQuery(req, id) {
-    let select = req.query.select;
-    if (!select) select = '*';
-    let table = req.params.table;
-    let orderBy = req.query.orderby;
-    if (!orderBy) orderBy = '';
-    else orderBy = 'ORDER BY ' + orderBy;
-    if (!id)
-     return `SELECT ${select} FROM ${table} ${orderBy}`;
-    else
-      return `SELECT ${select} FROM ${table} WHERE ${table.slice(0,-1)}id=${id} ${orderBy}`;
-  }
+  let select = req.query.select;
+  if (!select) select = '*';
+  let table = req.params.table;
+  let orderBy = req.query.orderby;
+  if (!orderBy) orderBy = '';
+  else orderBy = 'ORDER BY ' + orderBy;
+  if (!id) return `SELECT ${select} FROM ${table} ${orderBy}`;
+  else
+    return `SELECT ${select} FROM ${table} WHERE ${table.slice(
+      0,
+      -1
+    )}id=${id} ${orderBy}`;
+}
