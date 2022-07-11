789c7751ed39f925b09b231ba35d6989521594b0
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-11 18:14:13 +0200

    Added handler for bad requests

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 
 const musicRoutes = require('./routes/music');
 app.use('/music', musicRoutes);
-
+app.use((req, res, next) => {
+    res.status(404).send('Not found');
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000);
--- controllers/music.js
+++ controllers/music.js
@@ -2,22 +2,22 @@ const service = require('../services/music');
 
 exports.getAll = (req, res, next) => {
   service.getAll(req, results =>{
-    res.send(results);
+    res.status(results.code).send(results.results);
   });
 };
 exports.getById = (req, res, next) => {
   service.getById(req, results =>{
-    res.send(results);
+    res.status(results.code).send(results.results);
   });
 };
 exports.getArtistsByGenre = (req, res, next) => {
   service.getArtistsByGenre(req, results =>{
-    res.send(results);
+    res.status(results.code).send(results.results);
   });
 };
 exports.getArtistsWithTrackCount = (req, res, next) => {
   service.getArtistsWithTrackCount(req, results =>{
-    res.send(results);
+    res.status(results.code).send(results.results);
   });
 };
 
