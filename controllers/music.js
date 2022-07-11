1459c9c7841288b57dbf7329c06a11b2e046c050
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-11 14:57:43 +0200

    Created findArtistsWithTrackCount endpoint

--- controllers/music.js
+++ controllers/music.js
@@ -9,3 +9,13 @@ exports.getArtistsByGenre = (req, res, next) => {
   // nesting way: artists->albums->tracks->genres
   // cte
 };
+exports.getArtistsWithTrackCount = (req, res, next) => {
+    const genreId = req.params.genreid;
+    res.send([
+      { id: 1, name: 'artist1', count: 3 },
+      { id: 2, name: 'artist2', count: 10 },
+    ]);
+    // TODO sql get artists with count tracks
+    // nesting way: artists + albums + tracks=>count
+    // cte
+  };
--- routes/music.js
+++ routes/music.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 // GET /music/findArtistsByGenre/:genreid
 router.get('/findArtistsByGenre/:genreid', musicController.getArtistsByGenre);
+// GET /music/findArtistsWithTrackCount
+router.get('/findArtistsWithTrackCount', musicController.getArtistsWithTrackCount);
 
 module.exports = router;
